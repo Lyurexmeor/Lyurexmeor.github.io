019351275f586388957ec8379881009ada0a56e5
Lyurexmeor <10dit23f2013@student.psp.edu.my>
2025-04-10 23:26:59 +0800

    Practical_Individual

---
+++ Javascript/function.js
@@ -0,0 +1,33 @@
+function addition(){
+    let A=5;
+    let B=3;
+
+    console.log(A + B);
+
+
+}
+function subtraction(A,B){
+
+    console.log(A-B);
+}
+function multiplication(){
+
+    let a=5
+    let b=3
+    let result =a*b;
+
+    //increment
+     //result++;
+     //decrement
+     result--;
+
+     console.log(result)
+}
+
+function divide(){
+
+    let A=144
+    let B=12
+
+    console.log(A/B)
+}
