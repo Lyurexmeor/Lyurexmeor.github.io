9141703d602e49caeed7ae98e1516f4c846e5925
Lyurexmeor <10dit23f2013@student.psp.edu.my>
2025-04-26 14:11:58 +0800

    learn_java

--- Javascript/Modulus.js
+++ Javascript/Modulus.js
@@ -6,8 +6,34 @@ function calModulus(){
         alert("This even number");
     }else if(num2==0){
         alert("this odd number");
-    }else
+    }else{
+        alert("this o")
+    }
 
+
+
+}
+
+function looping(){
+for (let i=1; i<=6;i--){
+    console.log("Countdown"+1);
+}
+}
+
+function NamaLooping(){
+    let student =["Quan","Fatin","Meor","Heng","Imran"];
+    console.log(student.length);
+
+    for(let i=0;i<student.length;i++){
+        console.log(greet(student[i]));
+
+
+    }
+
+
+}
+function greet(){
+   return "hello"+greet;
 
 
 }
